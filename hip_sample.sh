3f7d5d8d4709069aa01049479a3d26b524018c97
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-02-16 17:51:43 -0500

    Some debugging of the code object format.

--- hip_sample.sh
+++ hip_sample.sh
@@ -13,6 +13,26 @@ export GENCO_FLAGS="$HIPCCFLAGS"
 make -C hip_dev/samples/1_Utils/hipInfo hipInfo
 make -C hip_dev/samples/0_Intro/module_api
 
-# note: need to be on a node with a GPU for this to work:
 cd hip_dev/samples/0_Intro/module_api
-./launchKernelHcc.hip.out
+
+# some debugging of the object file format
+clang-offload-bundler --list --inputs=vcpy_kernel.code --type=o
+# host-x86_64-unknown-linux
+# hipv4-amdgcn-amd-amdhsa--gfx90a
+
+clang-offload-bundler --unbundle --inputs=vcpy_kernel.code --type=o --outputs=vcpy_kernel.unbundle_device.o --targets=hipv4-amdgcn-amd-amdhsa--gfx90a
+clang-offload-bundler --unbundle --inputs=vcpy_kernel.code --type=o --outputs=vcpy_kernel.unbundle_host.o --targets=host-x86_64-unknown-linux
+
+#llvm-dis vcpy_kernel.unbundle_device.o
+# llvm-dis: error: file doesn't start with bitcode header
+
+nm vcpy_kernel.unbundle_device.o
+# 0000000000002000 d _DYNAMIC
+# 0000000000001000 T hello_world
+# 0000000000000680 R hello_world.kd
+
+file vcpy_kernel.unbundle_device.o
+# vcpy_kernel.unbundle_device.o: ELF 64-bit LSB shared object, version 1, dynamically linked, not stripped
+
+# note: need to be on a node with a GPU for this to work:
+# ./launchKernelHcc.hip.out
