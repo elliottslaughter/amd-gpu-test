558452b4c505a22911d7d5dcac7f6caf79ddc70f
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-02-16 11:54:40 -0800

    Scripts to build and run HIP sample code.

---
+++ hip_sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+source crusher_env.sh
+
+if [[ ! -e hip_dev ]]; then
+    git clone -b eds/testing https://github.com/elliottslaughter/HIP.git hip_dev
+fi
+
+cd hip_dev/samples/0_Intro/module_api
+make
