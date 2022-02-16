fa6d27801124d3e0e5398ce2a156bd5bdfc39144
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-02-16 15:14:44 -0500

    Run HIP sample.

--- hip_sample.sh
+++ hip_sample.sh
@@ -8,7 +8,11 @@ if [[ ! -e hip_dev ]]; then
     git clone -b eds/testing https://github.com/elliottslaughter/HIP.git hip_dev
 fi
 
-cd hip_dev/samples/0_Intro/module_api
 export HIPCC_FLAGS="$HIPCCFLAGS"
 export GENCO_FLAGS="$HIPCCFLAGS"
-make
+make -C hip_dev/samples/1_Utils/hipInfo hipInfo
+make -C hip_dev/samples/0_Intro/module_api
+
+# note: need to be on a node with a GPU for this to work:
+cd hip_dev/samples/0_Intro/module_api
+./launchKernelHcc.hip.out
